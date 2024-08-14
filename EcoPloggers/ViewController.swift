12edb35b00f79bdbc7789d8a78e1c8e15fc8563b
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-08-14 20:55:24 +0900

    [Setting] 프로젝트 추가 및 초기 설정 (#1)
    * [Setting] 프로젝트 추가
    
    * [Setting] 프로젝트 초기 설정

---
+++ EcoPloggers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EcoPloggers
+//
+//  Created by Jisoo HAM on 8/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
